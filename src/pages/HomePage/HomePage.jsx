ace61ca3dc790a80370e0f328a0ed04ebb01d028
vygantaspaul <wygantas.pauliukonis@gmail.com>
2024-09-26 17:59:49 +0300

    filter categories html

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,15 +4,26 @@ import React, {
 import {
     UserContext
 } from "../../components/UserContext/UserContext";
-import styles from './HomePage.module.css'
+import styles
+    from './HomePage.module.css'
+import CategoryList
+    from "../../components/category/CategoryList.jsx";
+
+import CategoryCard
+    from "../../components/category/CategoryCard.jsx";
+
 export const HomePage = () => {
     const { user } = useContext( UserContext );
 
     return (
-        <div className={styles.homePage}>
+        <div className={ styles.homePage }>
             <div className="container">
+
                 <section className="home-content">
                     <h1>Home Page</h1>
+                    <CategoryList/>
+                    <h2>Popular categories</h2>
+                    <CategoryCard/>
                     { user ? (
                         <p>Welcome back, { user.username }!</p>
                     ) : (
